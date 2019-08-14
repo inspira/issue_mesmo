ec9750f2534b86b184d61c3a1f5f158db7aec291
Camila Azuma <camilaazuma@outlook.com>
2019-08-14 15:30:11 -0300

    exercício zerado

--- popup.js
+++ popup.js
@@ -1,10 +1,8 @@
 'use strict';
 
-let activityLacbel = document.getElementById("activity");
+let activityLabel = document.getElementById("activity");
 let messageLabel = document.getElementById("message");
 let startButton = document.getElementById("start");
-let stopButton = document.getElementById("stop");
-let duracaoSpan = document.getElementById("duration");
 
 // 🥄: função que retorna código da issue do jira
 const getIssueCode = (url) => {
@@ -30,40 +28,12 @@ chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 
   // 🥄: exemplo de onclick
   startButton.onclick = function(){
-    
-    var issueCode = getIssueCode(url);
-    let startDate = new Date();
-    
-    chrome.storage.sync.set({"issue": issueCode, "dateStart": startDate}, function() {
-      let startDateFormatted = startDate.toLocaleString();
-      message.innerHTML = 'Value is set to ' + issueCode + " / startDate: " + startDateFormatted;
-      console.log('Value is set to ' + issueCode);
-      
-    });
+    message.innerHTML = "clicadinho :)";
   };
   
   /* ✨ Façam a mágica aqui ✨*/
 
-  stopButton.onclick = function(){
-    
-    var issueCode = getIssueCode(url);
-    let stopTime = new Date();
-     chrome.storage.sync.get('dateStart', function(olderDate) {
-      
-      let stopTimeFormatted = stopTime.toLocaleString();
-      message.innerHTML = " start: " + JSON.stringify(olderDate.dateStart);
-      var duracao = difference_times(stopTime, olderDate.dateStart);
-      duracaoSpan.innerHTML = duracao;
-    });
-    
- 
-  };
-
-function difference_times(newerDate, olderDate){
   
-  var milliseconds = (newerDate - olderDate);
-  return Math.round((milliseconds % 86400000) / 60000);
-}
 
 
   
